5fbedddb875aef694661eb7763a3c562c0cf6346
tstaten56 <tstaten56@gmail.com>
2015-12-01 22:50:34 -0500

    Fixed Turret, UI and added rupees

--- 2DProject/Assets/Scripts/LinkControllerScript.cs
+++ 2DProject/Assets/Scripts/LinkControllerScript.cs
@@ -1,4 +1,190 @@
-﻿//using UnityEngine;
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class LinkControllerScript : MonoBehaviour
+{
+
+    //Floats
+    public float maxSpeed = 3;
+    public float speed = 50f;
+    public float jumpPower = 150f;
+
+    //Booleans
+    public bool grounded;
+    public bool canDoubleJump;
+
+    //Stats
+    public int curHealth;
+    public int maxHealth = 5;
+
+    //References
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+    private GameMaster gm;
+
+    void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
+
+        curHealth = maxHealth;
+        //Debug.Log(curHealth);
+        gm = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<GameMaster>();
+    }
+
+
+    void Update()
+    {
+
+
+        anim.SetBool("Ground", grounded);
+        anim.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
+
+        if (Input.GetAxis("Horizontal") < -0.1f)
+        {
+            transform.localScale = new Vector3(8, 8, 8);
+        }
+
+        if (Input.GetAxis("Horizontal") > 0.1f)
+        {
+            transform.localScale = new Vector3(-8, 8, 8);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (grounded)
+            {
+                rb2d.AddForce(Vector2.up * jumpPower);
+                canDoubleJump = true;
+            }
+            else
+            {
+
+                if (canDoubleJump)
+                {
+
+                    canDoubleJump = false;
+                    rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
+                    rb2d.AddForce(Vector2.up * jumpPower / 1.75f);
+
+                }
+
+            }
+        }
+
+        if (curHealth > maxHealth)
+        {
+            curHealth = maxHealth;
+        }
+
+        if (curHealth <= 0)
+        {
+
+            curHealth = 0;
+
+            Die();
+
+        }
+
+
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 easeVelocity = rb2d.velocity;
+        easeVelocity.y = rb2d.velocity.y;
+        easeVelocity.z = 0.0f;
+        easeVelocity.x *= 0.75f;
+
+        float h = Input.GetAxis("Horizontal");
+
+        //Fake friction / Easing the x speed of our player
+        if (grounded)
+        {
+
+            rb2d.velocity = easeVelocity;
+
+        }
+
+
+        //Moving the player
+        rb2d.AddForce((Vector2.right * speed) * h);
+
+        //Debug.Log(rb2d.velocity);
+        //Limiting the speed of the player
+        if (rb2d.velocity.x > maxSpeed)
+        {
+            rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
+        }
+
+        if (rb2d.velocity.x < -maxSpeed)
+        {
+            rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
+        }
+
+    }
+
+    void Die()
+    {
+
+        Application.LoadLevel(Application.loadedLevel);
+
+    }
+
+    public void Damage(int dmg)
+    {
+
+        curHealth -= dmg;
+        gameObject.GetComponent<Animation>().Play("RedFlash");
+
+    }
+
+
+    public IEnumerator Knockback(float knockDur, float knockbackPwr, Vector3 knockbackDir)
+    {
+        Debug.Log("Knockback");
+
+        float timer = 0;
+        rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
+        while (knockDur > timer)
+        {
+
+            timer += Time.deltaTime;
+            //rb2d.velocity = new Vector2(0, 0);
+            rb2d.AddForce(new Vector3(knockbackDir.x * -100, knockbackDir.y * knockbackPwr, transform.position.z));
+
+        }
+
+        yield return 0;
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.CompareTag("Rupee"))
+        {
+            Destroy(col.gameObject);
+            gm.rupees += 1;
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//using UnityEngine;
 //using System.Collections;
 
 //public class LinkControllerScript : MonoBehaviour {
@@ -179,169 +365,3 @@
 //        yield return 0; //In IEnumerator you have to return something, this stops the IEnumerator
 //    }
 //}
-
-using UnityEngine;
-using System.Collections;
-
-public class LinkControllerScript : MonoBehaviour
-{
-
-    //Floats
-    public float maxSpeed = 3;
-    public float speed = 50f;
-    public float jumpPower = 150f;
-
-    //Booleans
-    public bool grounded;
-    public bool canDoubleJump;
-
-    //Stats
-    public int curHealth;
-    public int maxHealth = 5;
-
-    //References
-    private Rigidbody2D rb2d;
-    private Animator anim;
-
-
-
-    void Start()
-    {
-        rb2d = gameObject.GetComponent<Rigidbody2D>();
-        anim = gameObject.GetComponent<Animator>();
-
-        curHealth = maxHealth;
-        Debug.Log(curHealth);
-
-    }
-
-
-    void Update()
-    {
-
-
-        anim.SetBool("Ground", grounded);
-        anim.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
-
-        if (Input.GetAxis("Horizontal") < -0.1f)
-        {
-            transform.localScale = new Vector3(8, 8, 8);
-        }
-
-        if (Input.GetAxis("Horizontal") > 0.1f)
-        {
-            transform.localScale = new Vector3(-8, 8, 8);
-        }
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            if (grounded)
-            {
-                rb2d.AddForce(Vector2.up * jumpPower);
-                canDoubleJump = true;
-            }
-            else
-            {
-
-                if (canDoubleJump)
-                {
-
-                    canDoubleJump = false;
-                    rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
-                    rb2d.AddForce(Vector2.up * jumpPower / 1.75f);
-
-                }
-
-            }
-        }
-
-        if (curHealth > maxHealth)
-        {
-            curHealth = maxHealth;
-        }
-
-        if (curHealth <= 0)
-        {
-
-            curHealth = 0;
-
-            Die();
-
-        }
-
-
-
-    }
-
-    void FixedUpdate()
-    {
-        Vector3 easeVelocity = rb2d.velocity;
-        easeVelocity.y = rb2d.velocity.y;
-        easeVelocity.z = 0.0f;
-        easeVelocity.x *= 0.75f;
-
-        float h = Input.GetAxis("Horizontal");
-
-        //Fake friction / Easing the x speed of our player
-        if (grounded)
-        {
-
-            rb2d.velocity = easeVelocity;
-
-        }
-
-
-        //Moving the player
-        rb2d.AddForce((Vector2.right * speed) * h);
-
-        //Debug.Log(rb2d.velocity);
-        //Limiting the speed of the player
-        if (rb2d.velocity.x > maxSpeed)
-        {
-            rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
-        }
-
-        if (rb2d.velocity.x < -maxSpeed)
-        {
-            rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
-        }
-
-    }
-
-    void Die()
-    {
-
-        Application.LoadLevel(Application.loadedLevel);
-
-    }
-
-    public void Damage(int dmg)
-    {
-
-        curHealth -= dmg;
-        gameObject.GetComponent<Animation>().Play("RedFlash");
-
-    }
-
-
-    public IEnumerator Knockback(float knockDur, float knockbackPwr, Vector3 knockbackDir)
-    {
-        Debug.Log("Knockback");
-
-        float timer = 0;
-
-        while (knockDur > timer)
-        {
-
-            timer += Time.deltaTime;
-            rb2d.velocity = new Vector2(0, 0);
-            rb2d.AddForce(new Vector3(knockbackDir.x * -100, knockbackDir.y + knockbackPwr, transform.position.z));
-
-        }
-
-        yield return 0;
-
-    }
-
-
-}
---
+++ 2DProject/Assets/Scripts/TurretAI.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretAI : MonoBehaviour {
+
+    //Integers
+    public int curHealth;
+    public int maxHealth;
+
+    //Float
+    public float distance; //btwn player and turret
+    public float wakeRange;
+    public float shootInterval;
+    public float bulletSpd = 100;
+    public float bulletTimer;
+
+    //Boolean
+    public bool awake = false;
+    public bool lookingRight = true;
+
+    //References
+    public GameObject bullet;
+    public Transform target;
+    public Animator anim;
+    public Transform shootPointLeft;
+    public Transform shootPointRight;
+
+    void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        curHealth = maxHealth;
+
+
+    }
+
+    void Update()
+    {
+        anim.SetBool("Awake", awake);
+        anim.SetBool("LookingRight", lookingRight);
+
+        RangeCheck(); 
+
+        if(target.transform.position.x > transform.position.x)
+        {
+            lookingRight = true;
+        }
+
+        if(target.transform.position.x < transform.position.x)
+        {
+            lookingRight = false;
+        }
+
+        if(curHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void RangeCheck()
+    {
+        distance = Vector3.Distance(transform.position, target.transform.position); //get dist btwn turret and player
+
+        //wakeup when player is in awake range
+        if(distance < wakeRange)
+        {
+            awake = true;
+        }
+        //Go to sleep when player out of awake range
+        if (distance > wakeRange)
+        {
+            awake = false;
+        }
+    }
+
+    public void Attack(bool attackingRight)
+    {
+        bulletTimer += Time.deltaTime;
+
+        if(bulletTimer >= shootInterval)
+        {
+            Vector2 direction = target.transform.position - transform.position;
+            direction.Normalize();
+
+            if(!attackingRight)
+            {
+                GameObject bulletClone;
+                bulletClone = Instantiate(bullet, shootPointLeft.transform.position, shootPointLeft.transform.rotation) as GameObject;
+                bulletClone.GetComponent<Rigidbody2D>().velocity = direction * bulletSpd;
+
+                bulletTimer = 0;
+            }
+
+            if(attackingRight)
+            {
+                GameObject bulletClone;
+                bulletClone = Instantiate(bullet, shootPointRight.transform.position, shootPointRight.transform.rotation) as GameObject;
+                bulletClone.GetComponent<Rigidbody2D>().velocity = direction * bulletSpd;
+
+                bulletTimer = 0;
+            }
+            
+        }
+    }
+
+    public void Damage(int dmg)
+    {
+        curHealth -= dmg;
+        gameObject.GetComponent<Animation>().Play("RedFlash");
+    }
+
+}
